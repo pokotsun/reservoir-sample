2ee54f82729c61cc1548fbf8a854baedbda76f44
pokotsun <gorigolila@gmail.com>
2018-12-23 00:03:24 +0900

    rename varialbe

--- reservoir_network.py
+++ reservoir_network.py
@@ -4,41 +4,41 @@ class ReservoirNetWork:
 
     def __init__(self, inputs, num_input_nodes, num_reservoir_nodes, num_output_nodes, leak_rate=0.1, activator=np.tanh):
         self.inputs = inputs
-        self.nodes_reservoir = np.random.uniform(0, 1, num_reservoir_nodes)
+        self.reservoir_nodes = np.random.uniform(0, 1, num_reservoir_nodes)
         self.outputs = np.zeros(num_output_nodes)
 
         self.weights_input = self._generate_variational_weights(num_input_nodes, num_reservoir_nodes)
         self.weights_reservoir = self._generate_reservoir_weights(num_reservoir_nodes)
         self.weights_output = np.zeros([num_reservoir_nodes, num_output_nodes])
         
+        # 
         self.num_input_nodes = num_input_nodes
         self.num_reservoir_nodes = num_reservoir_nodes
         self.num_output_nodes = num_output_nodes
-        
+
         self.leak_rate = leak_rate
         self.activator = activator
 
-    # 重みを作成
+    # create weights which variated by -0.1 or +0.1
     def _generate_variational_weights(self, num_pre_nodes, num_post_nodes):
-        return np.random.randint(0, 2, num_pre_nodes * num_post_nodes).reshape([num_pre_nodes, num_post_nodes]) * 2 - 1
+        return (np.random.randint(0, 2, num_pre_nodes * num_post_nodes).reshape([num_pre_nodes, num_post_nodes]) * 2 - 1) * 0.1
     
     # Reservoir層の重みを作成
-    def _generate_reservoir_weights(self, num_nodes):
+    def _generate_reservoir_weights(self, num_nodes, scale_weights_reservoir=0.9):
         weights = np.random.normal(0, 1, num_nodes * num_nodes).reshape([num_nodes, num_nodes])
         spectral_radius = max(abs(linalg.eigvals(weights)))
-        weights / spectral_radius * 0.9
-        return weights
+        return (weights / spectral_radius) * scale_weights_reservoir
 
-    # def print_weights(self):
-    #     print(f"weights_input:\n{self.weights_input}\n")
-    #     print(f"weights_reservoir:\n{self.weights_reservoir}\n")
-    #     print(f"weights_output:\n{self.weights_output}\n")
+    def print_weights(self):
+        print(f"weights_input:\n{self.weights_input}\n")
+        print(f"weights_reservoir:\n{self.weights_reservoir}\n")
+        print(f"weights_output:\n{self.weights_output}\n")
 
     def train(self, lambda0=0.1):
         for input in self.inputs:
-            current_x = (1 - self.leak_rate) * self.nodes_reservoir
+            current_x = (1 - self.leak_rate) * self.reservoir_nodes
             current_x += self.leak_rate * (np.array([input]) @ self.weights_input
-             + self.nodes_reservoir @ self.weights_reservoir)
+             + self.reservoir_nodes @ self.weights_reservoir)
             current_x = self.activator(current_x)
             
             # Ridge Regression
