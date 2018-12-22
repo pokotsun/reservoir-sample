9055e3aff51b0cf9eaf5cad86739820fc0f74c8d
pokotsun <gorigolila@gmail.com>
2018-12-22 21:42:10 +0900

    create fit function

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import numpy as np
+from input_generator import InputGenerator
 from reservoir_network import ReservoirNetWork
 
 
+NUM_TIME_STEPS = 10
+TIME_STEP = 0.5
 def main():
     print(f"Hello, My Reservoir Network!!")
-    rn = ReservoirNetWork(np.arange(10), 10, 10, 1)
+    inputs = InputGenerator(NUM_TIME_STEPS, TIME_STEP).generate_sin()
+    print(f"inputs: {inputs}")
+    rn = ReservoirNetWork(np.arange(10), 5, 5, 1)
+    print(rn.print_weights())
 
 if __name__=="__main__":
     main()
+
 
 
 
--- reservoir_network.py
+++ reservoir_network.py
@@ -2,20 +2,39 @@ import numpy as np
 
 class ReservoirNetWork:
 
-    def __init__(self, inputs, num_input_nodes, num_reservoir_layer_nodes, num_output_nodes):
+    def __init__(self, inputs, num_input_nodes, num_reservoir_nodes, num_output_nodes):
         self.inputs = inputs
-        self.weights_input = self._generate_variational_weights(num_input_nodes)
-        self.weights_reservoir = self._generate_reservoir_weights(num_reservoir_layer_nodes)
-        self.weights_output = np.zeros(num_output_nodes)
+        self.nodes_reservoir = np.random.uniform(0, 1, num_reservoir_nodes)
+        self.outputs = np.zeros(num_output_nodes)
+        self.weights_input = self._generate_variational_weights(num_input_nodes, num_reservoir_nodes)
+        self.weights_reservoir = self._generate_reservoir_weights(num_reservoir_nodes)
+        self.weights_output = np.zeros([num_reservoir_nodes, num_output_nodes])
 
     # 重みを作成
-    def _generate_variational_weights(self, num_nodes):
-        return np.random.randint(0, 2, num_nodes) * 2 - 1
+    def _generate_variational_weights(self, num_pre_nodes, num_post_nodes):
+        return np.random.randint(0, 2, num_pre_nodes * num_post_nodes).reshape([num_pre_nodes, num_post_nodes]) * 2 - 1
     
     # Reservoir層の重みを作成
     def _generate_reservoir_weights(self, num_nodes):
         weights = np.random.normal(0, 1, num_nodes * num_nodes).reshape([num_nodes, num_nodes])
         return weights
+
+    def print_weights(self):
+        print(f"weights_input: {self.weights_input}")
+        print(f"weights_reservoir: {self.weights_reservoir}")
+        print(f"weights_output: {self.weights_output}")
+
+    def fit(self):
+        for input in self.inputs:
+            x = input @ self.weights_input @ self.nodes_reservoir @ self.weights_reservoir @ self.weights_output
+            # Ridge Regression
+            lambda0 = np.identity(self.nodes_reservoir) * 0.1 # lambda0
+            inv_x = np.linalg.inv(x.T @ x + lambda0)
+            self.weights_output = (inv_x @ x.T @ input).T
+
+
+
+
     
     
 
