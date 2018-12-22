0aa00723b6ed8a53f83efc8fb392664efc73918d
pokotsun <gorigolila@gmail.com>
2018-12-23 03:34:29 +0900

    rename scale of inputs

--- reservoir_network.py
+++ reservoir_network.py
@@ -3,9 +3,10 @@ from scipy import linalg
 
 class ReservoirNetWork:
 
-    def __init__(self, inputs, num_input_nodes, num_reservoir_nodes, num_output_nodes, leak_rate=0.1, activator=np.tanh):
-        self.inputs = inputs
-        self.reservoir_nodes = np.random.uniform(0, 1, num_reservoir_nodes)
+    def __init__(self, inputs, num_input_nodes, num_reservoir_nodes, num_output_nodes, leak_rate=0.1, activator=np.tanh, scale_of_inputs=1.0):
+        self.inputs = inputs * scale_of_inputs
+        self.reservoir_nodes = np.zeros(num_reservoir_nodes)
+
         self.outputs = np.array([])
 
         self.weights_input = self._generate_variational_weights(num_input_nodes, num_reservoir_nodes)
@@ -25,10 +26,10 @@ class ReservoirNetWork:
         return (np.random.randint(0, 2, num_pre_nodes * num_post_nodes).reshape([num_pre_nodes, num_post_nodes]) * 2 - 1) * 0.1
     
     # create weights of reservoir layer
-    def _generate_reservoir_weights(self, num_nodes, scale_weights_reservoir=0.9):
+    def _generate_reservoir_weights(self, num_nodes):
         weights = np.random.normal(0, 1, num_nodes * num_nodes).reshape([num_nodes, num_nodes])
         spectral_radius = max(abs(linalg.eigvals(weights)))
-        return (weights / spectral_radius) * scale_weights_reservoir
+        return weights / spectral_radius
 
     def print_weights(self):
         print(f"weights_input:\n{self.weights_input}\n")
@@ -36,13 +37,13 @@ class ReservoirNetWork:
         print(f"weights_output:\n{self.weights_output}\n")
 
     def _update_reservoir_nodes(self, input):
-        current_x = (1 - self.leak_rate) * self.reservoir_nodes
-        current_x += self.leak_rate * (np.array([input]) @ self.weights_input
+        next_reservoir_nodes = (1 - self.leak_rate) * self.reservoir_nodes
+        next_reservoir_nodes += self.leak_rate * (np.array([input]) @ self.weights_input
             + self.reservoir_nodes @ self.weights_reservoir)
-        current_x = self.activator(current_x)
-        self.reservoir_nodes = current_x
+        self.reservoir_nodes = self.activator(next_reservoir_nodes)
+        
 
-    def _update_weights_output(self, input, lambda0=0.1):
+    def _update_weights_output(self, input, lambda0):
         # Ridge Regression
         E_lambda0 = np.identity(self.num_reservoir_nodes) * lambda0 # lambda0
         inv_x = np.linalg.inv(self.reservoir_nodes.T @ self.reservoir_nodes + E_lambda0)
