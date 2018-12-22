4447ae2be9e261a85b26ac853308f13da37eda09
pokotsun <gorigolila@gmail.com>
2018-12-22 19:50:07 +0900

    append pylint to dev packages

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+from reservoir_network import ReservoirNetWork
+
+print(f"Hello, My Reservoir")
+print(f"{ReservoirNetWork().inputs}")
+
+
+
+
---
+++ reservoir_network.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+class ReservoirNetWork:
+
+    def __init__(self):
+        self.inputs = np.zeros([10])
