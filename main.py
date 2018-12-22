704e8ac2af8fdaeef93c02bc0de8d7c7a092b53c
pokotsun <gorigolila@gmail.com>
2018-12-22 20:39:35 +0900

    weight creator

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import numpy as np
 from reservoir_network import ReservoirNetWork
 
-print(f"Hello, My Reservoir Network!!")
 
-rn = ReservoirNetWork()
+def main():
+    print(f"Hello, My Reservoir Network!!")
+    rn = ReservoirNetWork(np.arange(10), 10, 10, 1)
+
+if __name__=="__main__":
+    main()
+
+
+
 
 
 
--- reservoir_network.py
+++ reservoir_network.py
@@ -2,5 +2,20 @@ import numpy as np
 
 class ReservoirNetWork:
 
-    def __init__(self):
-        self.inputs = np.zeros([10])
+    def __init__(self, inputs, num_input_nodes, num_reservoir_layer_nodes, num_output_nodes):
+        self.inputs = inputs
+        self.weights_input = self._generate_variational_weights(num_input_nodes)
+        self.weights_reservoir = self._generate_reservoir_weights(num_reservoir_layer_nodes)
+        self.weights_output = np.zeros(num_output_nodes)
+
+    # 重みを作成
+    def _generate_variational_weights(self, num_nodes):
+        return np.random.randint(0, 2, num_nodes) * 2 - 1
+    
+    # Reservoir層の重みを作成
+    def _generate_reservoir_weights(self, num_nodes):
+        weights = np.random.normal(0, 1, num_nodes * num_nodes).reshape([num_nodes, num_nodes])
+        return weights
+    
+    
+
