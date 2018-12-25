65e7f230426d5a23ee53ff673fb28961c8d3e249
pokotsun <gorigolila@gmail.com>
2018-12-25 18:11:23 +0900

    complete to train data

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ from input_generator import InputGenerator
 from reservoir_network import ReservoirNetWork
 import matplotlib.pyplot as plt
 
-T = np.pi * 12
+T = np.pi * 16
 RATIO_TRAIN = 0.8
-dt = np.pi * 0.05
+# dt = np.pi * 1
+dt = np.pi * 0.06
 AMPLITUDE = 0.9
 LEAK_RATE=0.1
-NUM_RESERVOIR_NODES = 200
+NUM_RESERVOIR_NODES = 50
 
 def main():
     i_gen = InputGenerator(0, T, dt)
     data = i_gen.generate_sin(amplitude=AMPLITUDE)
     NUM_TRAIN = int(len(data) * RATIO_TRAIN)
+    print(f"NUM_TRAIN: {NUM_TRAIN}")
     train_data = data[:NUM_TRAIN][:]
 
     model = ReservoirNetWork(inputs=train_data,
@@ -22,18 +24,16 @@ def main():
         num_output_nodes=1, 
         leak_rate=LEAK_RATE)
 
-    trained_data = model.train()
-    print(f"MINIMUM_SQUARE_ERROR: {model.minimum_square_error(trained_data, train_data)}")
+    model.train()
+    trained_data = model.get_train_result()
+    print(f"desired_data:\n {len(train_data)}")
+    print(f"trained_data:\n {len(trained_data)}")
+    print(f"desired_max: {max(train_data)}")
+    print(f"trained_max: {max(trained_data)}")
+    print(f"desired_data: {np.array(trained_data) * max(train_data) / max(trained_data)}")
     
-    # predict_step = int((PREDICT_T - T) / dt)
-    # predicted_data = model.predict(predict_step)
-    predict_data = data[NUM_TRAIN:]
-    predict_step = len(predict_data)
-    predicted_data = model.predict(predict_step)
-    print(f"PREDICT_MINIMUM: {model.minimum_square_error(predicted_data, predict_data)}")
-    
-    plt.plot(np.arange(0, T, dt), data, label="inputs")
-    plt.plot(np.arange(0, T, dt), np.append(trained_data, predicted_data), label="outputs")
+    plt.plot(np.arange(0, int(T * RATIO_TRAIN), dt), train_data, label="inputs")
+    plt.plot(np.arange(0, int(T * RATIO_TRAIN), dt), np.array(trained_data) * max(train_data) / max(trained_data), label="outputs")
     plt.axvline(x=T, label="end of train", color="green")
     plt.legend()
     plt.title("Echo State Network Sin Prediction")
--- reservoir_network.py
+++ reservoir_network.py
@@ -5,9 +5,8 @@ class ReservoirNetWork:
 
     def __init__(self, inputs, num_input_nodes, num_reservoir_nodes, num_output_nodes, leak_rate=0.1, activator=np.tanh):
         self.inputs = inputs
-        self.reservoir_nodes = np.zeros(num_reservoir_nodes)
-
-        self.outputs = np.array([])
+        # self.reservoir_nodes = np.zeros(num_reservoir_nodes)
+        self.internal_states = np.array([np.zeros(num_reservoir_nodes)])
 
         self.weights_input = self._generate_variational_weights(num_input_nodes, num_reservoir_nodes)
         self.weights_reservoir = self._generate_reservoir_weights(num_reservoir_nodes)
@@ -21,6 +20,66 @@ class ReservoirNetWork:
         self.leak_rate = leak_rate
         self.activator = activator
 
+    def _get_next_reservoir_nodes(self, input, current_state):
+        next_state = (1 - self.leak_rate) * current_state
+        next_state += self.leak_rate * (np.array([input]) @ self.weights_input
+            + current_state @ self.weights_reservoir)
+        return self.activator(next_state)
+
+    def _update_weights_output(self, lambda0):
+        # Ridge Regression
+        E_lambda0 = np.identity(self.num_reservoir_nodes) * lambda0 # lambda0
+        inv_x = np.linalg.inv(self.internal_states.T @ self.internal_states + E_lambda0)
+        # update weights of output layer
+        self.weights_output = (inv_x @ self.internal_states.T) @ self.inputs
+
+    def train(self, lambda0=0.1):
+        for input in self.inputs:
+            current_state = np.array(self.internal_states[-1])
+            self.internal_states = np.append(self.internal_states,
+                [self._get_next_reservoir_nodes(input, current_state)], axis=0)
+        self.internal_states = self.internal_states[1:]
+        self._update_weights_output(lambda0)
+
+    def get_train_result(self):
+        outputs = []
+        reservoir_nodes = np.zeros(self.num_reservoir_nodes)
+        for input in self.inputs:
+            reservoir_nodes = self._get_next_reservoir_nodes(input, reservoir_nodes)
+            # print(f"reservoir_nodes: {reservoir_nodes}")
+            outputs.append(self.get_output(reservoir_nodes))
+        # print(f"internal_states: {self.internal_states}")
+        return outputs
+
+    def predict(self, length_of_sequence, lambda0=0.01):
+        predicted_outputs = np.array([self.inputs[-1]])
+
+        return predicted_outputs
+
+    # def predict(self, length_of_sequence, lambda0=0.1):
+    #     predicted_outputs = np.array([self.inputs[-1]])
+
+    #     for _ in range(length_of_sequence - 1):
+    #         last_output = predicted_outputs[-1]
+    #         self._update_reservoir_nodes(last_output)
+    #         # self._update_weights_output(last_output, lambda0)
+    #         predicted_output = self.get_current_output()
+    #         predicted_outputs = np.append(predicted_outputs, predicted_output)
+
+    #     return predicted_outputs
+
+    # get output of current state
+    def get_output(self, reservoir_nodes):
+        return self.activator(reservoir_nodes @ self.weights_output)
+
+    def get_next_output(self):
+        return self.activator(self.internal_states[-1] @ self.weights_output)
+    
+    def minimum_square_error(self, data_a, data_b):
+        return np.sqrt(np.sum((data_a - data_b) ** 2)) / len(data_a)
+
+
+    ##### private method ########
     # create weights either -0.1 or +0.1
     def _generate_variational_weights(self, num_pre_nodes, num_post_nodes):
         return (np.random.randint(0, 2, num_pre_nodes * num_post_nodes).reshape([num_pre_nodes, num_post_nodes]) * 2 - 1) * 0.1
@@ -30,54 +89,6 @@ class ReservoirNetWork:
         weights = np.random.normal(0, 1, num_nodes * num_nodes).reshape([num_nodes, num_nodes])
         spectral_radius = max(abs(linalg.eigvals(weights)))
         return weights / spectral_radius
-
-    def print_weights(self):
-        print(f"weights_input:\n{self.weights_input}\n")
-        print(f"weights_reservoir:\n{self.weights_reservoir}\n")
-        print(f"weights_output:\n{self.weights_output}\n")
-
-    def _update_reservoir_nodes(self, input):
-        next_reservoir_nodes = (1 - self.leak_rate) * self.reservoir_nodes
-        next_reservoir_nodes += self.leak_rate * (np.array([input]) @ self.weights_input
-            + self.reservoir_nodes @ self.weights_reservoir)
-        self.reservoir_nodes = self.activator(next_reservoir_nodes)
-        
-
-    def _update_weights_output(self, input, lambda0):
-        # Ridge Regression
-        E_lambda0 = np.identity(self.num_reservoir_nodes) * lambda0 # lambda0
-        inv_x = np.linalg.inv(self.reservoir_nodes.T @ self.reservoir_nodes + E_lambda0)
-        # update weights of output layer
-        self.weights_output = (inv_x @ self.reservoir_nodes.T).reshape([self.num_reservoir_nodes, self.num_output_nodes]) @ np.array([input])
-
-    def train(self, lambda0=0.1):
-        for input in self.inputs:
-            self._update_reservoir_nodes(input)
-            self._update_weights_output(input, lambda0)
-            # get trained output
-            output = self.get_current_output()
-            self.outputs = np.append(self.outputs, output)
-
-        return self.outputs
-    
-    def predict(self, length_of_sequence, lambda0=0.1):
-        predicted_outputs = np.array([self.inputs[-1]])
-
-        for _ in range(length_of_sequence - 1):
-            last_output = predicted_outputs[-1]
-            self._update_reservoir_nodes(last_output)
-            self._update_weights_output(last_output, lambda0)
-            predicted_output = self.get_current_output()
-            predicted_outputs = np.append(predicted_outputs, predicted_output)
-
-        return predicted_outputs
-
-    # get output of current state
-    def get_current_output(self):
-        return self.activator(self.reservoir_nodes @ self.weights_output)
-    
-    def minimum_square_error(self, data_a, data_b):
-        return np.sqrt(np.sum((data_a - data_b) ** 2)) / len(data_a)
         
 
 
