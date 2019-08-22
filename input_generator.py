0669be5e32b1035024c56dacbc2a17b2ff7fb5a2
pokotsun <gorigolila@gmail.com>
2019-08-23 00:52:09 +0900

    implement rossler to input generator

--- input_generator.py
+++ input_generator.py
@@ -10,5 +10,16 @@ class InputGenerator:
     
     def generate_sin(self, amplitude=1.0):
         return np.sin(np.arange(self.start_time, self.end_time, self.dt)) * amplitude
+
+    def generate_rossler(self, a=0.2, b=0.2, c=5.7):
+        data = [(0,0,0)]
+        for t in np.arange(self.dt, self.end_time, self.dt):
+            (px, py, pz) = data[-1]
+            x = px + self.dt * (-py - pz)
+            y = py + self.dt * (px + a * py)
+            z = pz + self.dt * (b + pz * (px - c))
+            data.append((x, y, z))
+        return data
+
     
 
